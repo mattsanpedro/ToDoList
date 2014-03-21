632036b001b74383422855b55159b193ef895cf1
Matt San Pedro <work.matt.sp@gmail.com>
2014-03-21 15:36:47 +0800

    Update Itinerary finished

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -6,19 +6,33 @@ class ItinerariesController < ApplicationController
 		@itineraries = Itinerary.all
 	end
 	def new
+		@itinerary = Itinerary.new
 	end
 	def create
 		@itinerary = Itinerary.new(itinerary_params)
 		if @itinerary.save
 			redirect_to action: 'show', id: @itinerary.id
 		else
-		render action: 'new'
+			render action: 'new'
 		end	
 	end
 
-	private
+	def edit
+		@itinerary = Itinerary.find(params[:id])
+	end
+	def update
+		@itinerary = Itinerary.find(params[:id])
+		if @itinerary.update(itinerary_params)
+			redirect_to action: 'show', id: @itinerary.id
+		else
+			render action: 'edit'
+		end
+	end
 
+	private
+	
 	def itinerary_params
 		params.require(:itinerary).permit(:location, :travel_on)
 	end
+	
 end
